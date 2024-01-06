81e24bda6d09631d7c881aa73ba0ec0157af5cc1
safderun <burakberkkeskin@gmail.com>
2024-01-06 18:40:29 +0300

    added jwt to the main app

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"safblog-backend/database"
 	"safblog-backend/router"
 
+	jwtware "github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -12,6 +13,9 @@ func main() {
 	database.Connect()
 
 	app := fiber.New()
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey: jwtware.SigningKey{Key: []byte("secret")},
+	}))
 	fmt.Println("fiber app created")
 	router.SetupRoutes(app)
 	app.Listen(":8080")
