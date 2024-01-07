010d4112e785d886af4c23166741016d31e461c0
safderun <burakberkkeskin@gmail.com>
2024-01-07 20:52:58 +0300

    added root user env values

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"safblog-backend/database"
 	"safblog-backend/router"
+	"safblog-backend/services"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	database.Connect()
-
 	app := fiber.New()
 	router.SetupRoutes(app)
+	_, err := services.CreateRootUser()
+	if err != nil {
+
+	}
 	app.Listen(":8080")
 }
--- services/authService.go
+++ services/authService.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"safblog-backend/config"
 	"safblog-backend/database"
 	"safblog-backend/models"
 	"strconv"
@@ -44,7 +45,6 @@ func CreateUser(registeredUser models.RegisterModel) (models.Response, error) {
 		error := "error while hashing the password"
 		return models.Response{Message: "failed to has password", Error: error}, errors.New(error)
 	}
-
 	user.Password = hash
 
 	err = db.Create(&user).Error
@@ -120,4 +120,44 @@ func LoginUser(loginUser models.LoginUser) (models.Response, error) {
 			"token": t,
 		},
 	}, nil
+}
+
+func CreateRootUser() (models.Response, error) {
+	fmt.Println("Creating root user.")
+	rootUserUsername := config.Config("ROOT_USER_USERNAME")
+	rootUserEmail := config.Config("ROOT_USER_EMAIL")
+	rootUserPassword := config.Config("ROOT_USER_PASSWORD")
+
+	user := models.User{
+		Username: rootUserUsername,
+		Email:    rootUserEmail,
+		Password: rootUserPassword,
+		IsAdmin:  true,
+	}
+
+	db := database.DB.Db
+
+	var dbEmailUser models.User
+	db.Find(&dbEmailUser, "email = ?", rootUserEmail)
+	if dbEmailUser.Email != "" {
+		err := "root user email already exists"
+		fmt.Println("")
+		return models.Response{Message: "failed to create root user", Error: err}, nil
+	}
+
+	hash, err := saltAndHash(rootUserPassword)
+	if err != nil {
+		error := "error while hashing the root user password"
+		return models.Response{Message: "failed to hash root user password", Error: error}, errors.New(error)
+	}
+	user.Password = hash
+
+	err = db.Create(&user).Error
+	if err != nil {
+		error := "could not create the root user"
+		return models.Response{Message: "failed to create root user", Error: error}, errors.New(error)
+	}
+
+	return SuccessResponse("admin user created", fiber.Map{"message": "admin user created"}), nil
+	//return services.SuccessResponse("admin user created")
 }
